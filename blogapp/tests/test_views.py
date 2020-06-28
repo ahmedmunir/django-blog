7895f457052419fa644b69ed0dfc94dcc6869e5c
ahmedmunir <mryagamie@outlook.com>
2020-06-28 07:20:56 +0200

    Database and makemigrations

---
+++ blogapp/tests/test_views.py
@@ -0,0 +1,37 @@
+from django.urls import reverse
+
+from django.contrib.auth.models import User
+
+from blogapp.models import Post
+
+from django.test import TestCase, Client
+
+class testViews(TestCase):
+
+    def setUp(self):
+        """ Initialize some inputs for virutal Database"""
+        self.user = User.objects.create(username='Ahmed', email='ahmed@gmail.com', password='ahmed1234')
+
+        self.post = Post.objects.create(title='post', content='This is post created', author=self.user)
+
+        self.client = Client()
+
+    def test_get_home(self):
+        """ Testing get home page"""
+
+        response = self.client.get(reverse('blog-home'))
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertTemplateUsed(response, 'blog/home.html')
+
+        self.assertEqual(str(response.context['posts'][0]['title']), 'title')
+        self.assertEqual(str(response.context['posts'][0]['content']), 'This is post created')
+
+    def test_get_about(self):
+        """ Testing get about page """
+        response = self.client.get(reverse('blog-about'))
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertTemplateUsed(response, 'blog/about.html')
