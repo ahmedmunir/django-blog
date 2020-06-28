8ec78e33aef72a96a0f0d93b18aef6ce3f881de2
ahmedmunir <mryagamie@outlook.com>
2020-06-28 23:33:11 +0200

    fix bugs at forms tests

--- users/tests/test_forms.py
+++ users/tests/test_forms.py
@@ -1,5 +1,3 @@
-from users.forms import UserRegisterForm
-
 from django.test import TestCase, Client
 
 from django.urls import reverse, resolve
@@ -11,7 +9,7 @@ class TestForms(TestCase):
     def setUp(self):
         self.client = Client()
 
-    def test_register_right_data(self):
+    def test_register_post(self):
         """ Test if right data provided """
 
         url = reverse('users-register')
@@ -25,16 +23,17 @@ class TestForms(TestCase):
             'password2': 'testing1234',            
         })
 
-        self.assertEqual(right_register.status_code, 200)
+        self.assertEqual(right_register.status_code, 302)
         self.assertTemplateUsed(right_register, 'blog/home.html')
 
-        form = UserRegisterForm(data= {
+        wrong_register = self.client.post(url, {
             'first_name': 'mohsen',
             'last_name': 'ali',
-            'email': 'joker@gmail.com',
+            'email': 'joker',
             'username': 'joker',
             'password1': 'testing1234',
             'password2': 'testing1234',            
         })
 
-        self.assertTrue(form.is_valid)
+        self.assertEqual(right_register.status_code, 200)
+        self.assertTemplateUsed(right_register, 'users/register.html')
