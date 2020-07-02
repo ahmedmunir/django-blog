aac778a1458334ba30d26c6faeb48cc4ad7d072a
ahmedmunir <mryagamie@outlook.com>
2020-07-02 07:05:10 +0200

    create CRUD using CBV and generic CBV

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,17 +1,73 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
+from django.contrib import messages
 
 from blogapp.models import Post
 
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+from django.contrib.messages.views import SuccessMessageMixin
+
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+
 # Create your views here.
 
-def home(request):
+class PostListView(ListView):
+    model = Post
+    template_name = 'blogapp/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
-    context = {
-        "posts": Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, UpdateView):
+    model = Post
+    success_message = "You updated post Successfully"
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        #get post object that created
+        post = self.get_object()
+
+        if post.author == self.request.user:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        #get post object that created
+        post = self.get_object()
+
+        if post.author == self.request.user:
+            return True
+        return False
+
+    def delete(self, request, *args, **kwargs):
+        obj = self.get_object()
+        messages.warning(self.request, "You deleted post successfully")
+        return super(PostDeleteView, self).delete(request, *args, **kwargs)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blogapp/about.html')
