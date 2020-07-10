2bc8275244ce07a2b482d21ef7afc67b1449c5dd
ahmedmunir <mryagamie@outlook.com>
2020-07-10 18:46:41 +0200

    Convert adding new comment to be Ajax request rather than redirect to comment_form.html page

--- comments/urls.py
+++ comments/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from .views import CommentCreateView, CommentUpdateView, CommentDeleteView
+# from .views import postComment
 
 urlpatterns = [
     path('new/', CommentCreateView.as_view(), name='comment-create'),
-    path('<int:pk>/update', CommentUpdateView.as_view(), name='comment-update'),
-    path('<int:pk>/delete', CommentDeleteView.as_view(), name='comment-delete')
+    path('<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete')
 ]
