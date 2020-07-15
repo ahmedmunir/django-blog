e05ab26eb8c4028832cfdccb23e8324516c14167
ahmedmunir <mryagamie@outlook.com>
2020-07-15 02:34:01 +0200

    modify some configuration of facebook login at django app

--- blog/settings.py
+++ blog/settings.py
@@ -80,6 +80,10 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                #add social-auth-django processors
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
